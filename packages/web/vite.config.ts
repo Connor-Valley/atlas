a21cde9b4e58f8c6e31629e888736131f7e39067
Connor Valley <cvalley088@gmail.com>
2026-04-08 17:34:17 -0400

    allow setting port variables with pnpm dev

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -5,16 +5,16 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: "0.0.0.0",
-    port: 5173,
+    port: parseInt(process.env.PORT || "5173"),
     allowedHosts: true,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: `http://localhost:${process.env.API_PORT || "3000"}`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
       "/share": {
-        target: "http://localhost:3000",
+        target: `http://localhost:${process.env.API_PORT || "3000"}`,
         changeOrigin: true,
       },
     },
